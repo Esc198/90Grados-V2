11d58ede910e672c5696c277f2ef9d8cdf3c75dc
enriq <esc1007@alu.ubu.es>
2024-11-18 17:27:04 +0100

    implementado los constructores de OpcionNoDisponibleException

--- src/noventagrados/textui/excepcion/OpcionNoDisponibleException.java
+++ src/noventagrados/textui/excepcion/OpcionNoDisponibleException.java
@@ -1,5 +1,41 @@
 package noventagrados.textui.excepcion;
 
-public class OpcionNoDisponibleException extends Exception{
+public class OpcionNoDisponibleException extends Exception {
 
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OpcionNoDisponibleException() {
+		super();
+	}
+
+	/**
+	 * Constructor de la excepción.
+	 * 
+	 * @param mensaje Mensaje de error.
+	 */
+	public OpcionNoDisponibleException(String mensaje) {
+		super(mensaje);
+	}
+
+	/**
+	 * Constructor de la excepción.
+	 * 
+	 * @param mensaje Mensaje de error.
+	 * @param causa   Causa de la excepción.
+	 */
+	public OpcionNoDisponibleException(String mensaje, Throwable causa) {
+		super(mensaje, causa);
+	}
+
+	/**
+	 * Constructor de la excepción.
+	 * 
+	 * @param causa Causa de la excepción.
+	 */
+	public OpcionNoDisponibleException(Throwable causa) {
+		super(causa);
+	}
 }
